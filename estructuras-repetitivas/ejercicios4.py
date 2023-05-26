ff9b25902e5abec06a4f1c80aca70001187b84a1
Fede Krenn <fedekrenn@gmail.com>
2023-05-26 14:30:01 -0300

    Actualización

--- estructuras-repetitivas/ejercicios4.py
+++ estructuras-repetitivas/ejercicios4.py
@@ -1,41 +1,41 @@
 # Escribir un programa que pida al usuario una palabra y luego muestre por pantalla una a una las letras de la palabra introducida empezando por la última.
 
-# word = input('Ingrese una palabra:\n')
+import random
+word = input('Ingrese una palabra:\n')
 
-# for i in range(len(word)-1, -1, -1):
-#     print(word[i])
+for i in range(len(word)-1, -1, -1):
+    print(word[i])
 
 
 
 # Pide al usuario una cadena de texto e indica si es un palíndromo o no. Palíndromo es una palabra o frase que se lee igual en un sentido que en otro. Por ejemplo: oso, ala, radar, etc. NO es necesario usar bucles.
 
-# paragraph = input('Ingrese una cadena de texto:\n')
-# turned = paragraph[::-1]
+paragraph = input('Ingrese una cadena de texto:\n')
+turned = paragraph[::-1]
 
-# isPalindrome = paragraph == turned
+isPalindrome = paragraph == turned
 
-# print('Sí, es palíndromo!' if isPalindrome else 'No, no es palíndromo')
+print('Sí, es palíndromo!' if isPalindrome else 'No, no es palíndromo')
 
 
 
 # Generaremos un número entre 1 y 10. Nuestro objetivo es adivinar el número. Si fallamos nos dirán si es mayor o menor que el número buscado. También poner el número de intentos requeridos.
 
-# import random
-# number = random.randint(1, 10)
+number = random.randint(1, 10)
+attemps = 1
 
-# attemps = 1
+while True:
+    user_num = int(input('Ingresa un número:\n'))
 
-# while True:
-#     user_num = int(input('Ingresa un número:\n'))
+    if number == user_num:
+        print('Ganaste!!')
+        break
+    else:
+        print('Mmm... no, el número es más alto' if number >
+              user_num else 'Mmm... no, el número es más bajo')
+        attemps += 1
 
-#     if number == user_num:
-#         print('Ganaste!!')
-#         break
-#     else: 
-#         print('Mmm... no, el número es más alto' if number > user_num else 'Mmm... no, el número es más bajo')
-#         attemps += 1
-
-# print(f'Ganaste en {attemps} intentos')
+print(f'Ganaste en {attemps} intentos')
 
 
 
@@ -49,16 +49,17 @@
 
 courses_for_sale = ["JavaScript", "c++", "Python", "Java", "php", "Go", "Rust", "Dart"]
 
-for index in range(len(courses_for_sale) -1, -1, -1):
+for index in range(len(courses_for_sale) - 1, -1, -1):
     print(courses_for_sale[index])
 
 while True:
-    user_course = input('-------\nIntroduce el curso que te gustaría tomar, escribe "exit" para salir:\n')
+    user_course = input(
+        '-------\nIntroduce el curso que te gustaría tomar, escribe "exit" para salir:\n')
 
     if user_course == 'exit':
         print('Gracias por su visita! Programa finalizado')
         break
     elif user_course in courses_for_sale:
-        print('\t -> Sí, ese curso está en nuestra plataforma')
+        print('\t ✅ Sí, ese curso está en nuestra plataforma')
     else:
-        print('\t -> Por el momento no tenemos esa propuesta')
+        print('\t ❌ Por el momento no tenemos esa propuesta')
